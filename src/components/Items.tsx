1bea2990e4b4c0a3ce3bd0b1d901a393a8974265
Aiden <aidenbenise@gmail.com>
2025-03-30 15:38:48 -0400

    added cards and improved navbar pt 2

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -16,16 +16,16 @@ const Items = ({ items }: ItemsProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {items.map((item) => (
-        <div key={item._id} className="bg-white border shadow-lg p-4 rounded-lg h-[450px] w-[250px]"> 
+        <div key={item._id} className="bg-white border shadow-lg p-4 rounded-lg h-[380px] w-[250px]"> 
           {/* Adjusting the card to a fixed height and width */}
           <img
             src={item.url}
             alt={item.title}
-            className="w-full h-[300px] object-cover rounded-md"
+            className="w-full h-[300px]"
             // Adjusted height for image to make it taller
           />
           <h3 className="text-xl font-semibold mt-4">{item.title}</h3>
-          <p className="text-gray-600 mt-2">{item.description}</p>
+
         </div>
       ))}
     </div>
